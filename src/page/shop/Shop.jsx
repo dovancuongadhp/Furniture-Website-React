7d29516db18419801a15521beb09a6d05c3272ed
CuongKun <dovancuongadhp@gmail.com>
2023-03-01 02:13:32 +0700

    css grid for product

--- src/page/shop/Shop.jsx
+++ src/page/shop/Shop.jsx
@@ -1,37 +1,46 @@
-import React from 'react'
-import TitleBar from '../../components/titlebar/TitleBar'
-import Category from '../../components/categories/Category'
-import '../../assets/style/shop.scss'
+import React from "react";
+import TitleBar from "../../components/titlebar/TitleBar";
+import Category from "../../components/categories/Category";
+import "../../assets/style/shop.scss";
+import CartIcon from "../../assets/icons/CartIcon";
 export default function Shop() {
   const listProduct = [
     {
       id: 1,
-      productName : 'chair123',
-      imgLink: 'https://images.gessato.com/cb:OWDa~b707/w:auto/h:auto/q:mauto/f:avif/https://www.gessato.com/wp-content/uploads/2023/01/dieckmann-typ-dieckmann-typ-chair-chair-7.jpg'
+      productName: "Ghe noi that dep",
+      price: 231,
+      imgLink:
+        "https://images.gessato.com/cb:OWDa~b707/w:auto/h:auto/q:mauto/f:avif/https://www.gessato.com/wp-content/uploads/2023/01/dieckmann-typ-dieckmann-typ-chair-chair-7.jpg",
     },
     {
       id: 2,
-      productName : 'chair123',
-      imgLink: 'https://images.gessato.com/cb:OWDa~b707/w:auto/h:auto/q:mauto/f:avif/https://www.gessato.com/wp-content/uploads/2023/01/dieckmann-typ-dieckmann-typ-chair-chair-7.jpg'
+      productName: "Ghe noi that dep",
+      price: 231,
+      imgLink:
+        "https://images.gessato.com/cb:OWDa~b707/w:auto/h:auto/q:mauto/f:avif/https://www.gessato.com/wp-content/uploads/2023/01/dieckmann-typ-dieckmann-typ-chair-chair-7.jpg",
     },
     {
       id: 3,
-      productName : 'chair123',
-      imgLink: 'https://images.gessato.com/cb:OWDa~b707/w:auto/h:auto/q:mauto/f:avif/https://www.gessato.com/wp-content/uploads/2023/01/dieckmann-typ-dieckmann-typ-chair-chair-7.jpg'
+      productName: "Ghe noi that dep",
+      price: 231,
+      imgLink:
+        "https://images.gessato.com/cb:OWDa~b707/w:auto/h:auto/q:mauto/f:avif/https://www.gessato.com/wp-content/uploads/2023/01/dieckmann-typ-dieckmann-typ-chair-chair-7.jpg",
     },
     {
       id: 4,
-      productName : 'chair123',
-      imgLink: 'https://images.gessato.com/cb:OWDa~b707/w:auto/h:auto/q:mauto/f:avif/https://www.gessato.com/wp-content/uploads/2023/01/dieckmann-typ-dieckmann-typ-chair-chair-7.jpg'
+      productName: "Ghe noi that dep",
+      price: 231,
+      imgLink:
+        "https://images.gessato.com/cb:OWDa~b707/w:auto/h:auto/q:mauto/f:avif/https://www.gessato.com/wp-content/uploads/2023/01/dieckmann-typ-dieckmann-typ-chair-chair-7.jpg",
     },
-  ]
+  ];
   return (
-    <div className='shop__container'> 
+    <div className="shop__container">
       <TitleBar namePage={"Shop"} />
       <div className="shop__inner">
         <div className="wrapper__product">
           <div className="filter__product">
-            <select name="sorting" id="sort" placeholder='Search'>
+            <select name="sorting" id="sort" placeholder="Search">
               <option value="popularity">Sort by popularity</option>
               <option value="rating">Sort by average rating</option>
               <option value="latest">Short by latest</option>
@@ -41,24 +50,28 @@ export default function Shop() {
           </div>
           <br />
           <div className="list__product">
-            {
-              listProduct.map(item => (
-                <div key={item.id}>
-                  <div className="product__wrapper">
-                    <div className="product__img">
-                      <img src={item.imgLink} alt="photo" />
-                      <p>{item.productName}</p>
-                    </div>
+            {listProduct.map((item) => (
+              <div key={item.id} className="product__wrapper">
+                <div className="product__img">
+                  <img src={item.imgLink} alt="photo" />
+                </div>
+                <div className="product__bottom">
+                  <div className="product__description">
+                    <p className="product__name">{item.productName}</p>
+                    <p className="product__price">{item.price}</p>
+                  </div>
+                  <div className="product__cart">
+                    <CartIcon />
                   </div>
                 </div>
-              ))
-            }
+              </div>
+            ))}
           </div>
         </div>
         <div className="shop__categories">
-          <Category/>
+          <Category />
         </div>
       </div>
     </div>
-  )
+  );
 }
