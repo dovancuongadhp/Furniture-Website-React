d0c9bf645bef712449ce5e9254646ae45ad4ac07
Do Cuong <cuongdkun@Dos-iMac.local>
2023-02-28 17:51:06 +0700

    reponsive navbar

--- src/page/shop/Shop.jsx
+++ src/page/shop/Shop.jsx
@@ -6,11 +6,23 @@ export default function Shop() {
   return (
     <div className='shop__container'> 
       <TitleBar namePage={"Shop"} />
-      <div className="shop__content">
-        <div className="list__product">
-          List product
+      <div className="shop__inner">
+        <div className="wrapper__product">
+          <div className="filter__product">
+            <select name="sorting" id="sort" placeholder='Search'>
+              <option value="popularity">Sort by popularity</option>
+              <option value="rating">Sort by average rating</option>
+              <option value="latest">Short by latest</option>
+              <option value="lowtohight">Short by price: low to hight</option>
+              <option value="highttolow">Short by price: hight to low</option>
+            </select>
+          </div>
+          <br />
+          <div className="list__product">
+            ListProduct
+          </div>
         </div>
-        <div className="categories">
+        <div className="shop__categories">
           <Category/>
         </div>
       </div>
