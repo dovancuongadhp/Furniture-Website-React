284736a1b0b91b57e84f12d8adcbe75165ef72b0
CuongKun <dovancuongadhp@gmail.com>
2023-03-02 19:31:22 +0700

    fit lay list-product shop

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,15 +1,8 @@
 import React from "react";
-import TitleBar from "../../components/titlebar/TitleBar";
-import Category from "../../components/categories/Category";
 import "../../assets/style/home.scss";
-import CartIcon from "../../assets/icons/CartIcon";
-import CardProduct from "../../components/cardProduct/CardProduct";
 export default function Home() {
   return(
     <div className="container__home">
-      <CardProduct image={"https://images.gessato.com/cb:OWDa~b707/w:auto/h:auto/q:mauto/f:avif/https://www.gessato.com/wp-content/uploads/2023/01/dieckmann-typ-dieckmann-typ-chair-chair-7.jpg"} price={1231} productName={"test 1231"}/>
-      <CardProduct image={"https://images.gessato.com/cb:OWDa~b707/w:auto/h:auto/q:mauto/f:avif/https://www.gessato.com/wp-content/uploads/2023/01/dieckmann-typ-dieckmann-typ-chair-chair-7.jpg"} price={1231} productName={"test 1231"}/>
-      <CardProduct image={"https://images.gessato.com/cb:OWDa~b707/w:auto/h:auto/q:mauto/f:avif/https://www.gessato.com/wp-content/uploads/2023/01/dieckmann-typ-dieckmann-typ-chair-chair-7.jpg"} price={1231} productName={"test 1231"}/>
     </div>
   )
 }
