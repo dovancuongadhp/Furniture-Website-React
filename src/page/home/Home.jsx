2e1ddec63888c15d4c316d7694f13e4cb9a044e9
Do Cuong <cuongdkun@Dos-iMac.local>
2023-03-07 14:47:16 +0700

    update image

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../../assets/style/home.scss";
-import ArmChairBg from '../../assets/img/home-page/arm-chair-bg.png'
-import ImgGreen from "../../assets/img/home-page/img-green.jpg";
-import ImgLamp from "../../assets/img/home-page/img-lamp.jpg";
-import ImgPink from "../../assets/img/home-page/img-pink.jpg";
+import ArmChairBg from "../../assets/img/home-page/arm-chair.png";
+import LayerBlue from "../../assets/img/home-page/layerBlue.jpg";
+import LayerGreen from "../../assets/img/home-page/layerGreen.jpg";
+import LayerOrange from "../../assets/img/home-page/layerOrange.jpg";
 import BrandSlide from "./brand-slide/BrandSlide";
 import CardCollection from "./card-collection/CardCollection";
+import GraySofaBrown from "../../assets/img/home-page/gray-sofa-brown.jpg";
 export default function Home() {
   return (
     <div className="home__container">
@@ -38,16 +39,19 @@ export default function Home() {
         </section>
         <section>
           <div className="page-two">
-            <CardCollection image={ImgLamp}/>
-            <CardCollection image={ImgPink}/>
-            <CardCollection image={ImgGreen}/>
+            <CardCollection image={LayerOrange} />
+            <CardCollection image={LayerGreen} />
+            <CardCollection image={LayerBlue} />
           </div>
         </section>
         <section className="brand-slide">
           <BrandSlide />
         </section>
-        <section className="page-three">
-
+        <section>
+          <div className="page-three">
+            <p>Featured products with amazing color variety for your home</p>
+            <img src={GraySofaBrown} alt="lamp" />
+          </div>
         </section>
       </div>
     </div>
